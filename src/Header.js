92d79755d920d4297b9fda49f310759d235db352
Eason <eysliang2-c@uowchk.edu.hk>
2020-06-23 11:35:45 +0800

    header is making

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.css';
+function Header() {
+    return(
+        <header id='header'>
+            123456
+        </header>
+    );
+
+}
+export default Header;
